ae800c0383d021979ae348b7782c841f52605dc8
Paul M Edwards <pauledwards@gmail.com>
2020-02-21 23:45:30 -0800

    Add moment datetime lib & fix all tests!

--- api/auth/auth.spec.js
+++ api/auth/auth.spec.js
@@ -4,16 +4,19 @@ const server = require('../server.js');
 const apiBase = '/api/auth';
 const json = 'application/json';
 
+const moment = require('moment');
+const d = moment().format("X");
+
 describe('server.js', () => {
 
-  let apiEndpoint = apiBase+'/register';
-  describe('POST '+apiEndpoint, () => {
+  const apiEndpointRegister = apiBase+'/register';
+  describe('POST '+apiEndpointRegister, () => {
 
     it('(missing required data)', async (done) => {
       const reqBody = null;
       const expStatus = 400;
       const resBody = { message: "Required data missing" };
-      const res = await supertest(server).post(apiEndpoint);
+      const res = await supertest(server).post(apiEndpointRegister);
       expect(res.status).toBe(expStatus);
       expect(res.type).toBe(json);
       expect(res.body).toEqual(resBody);
@@ -23,19 +26,17 @@ describe('server.js', () => {
 
     it('(valid)', async (done) => {
       const password = "T3$tP4$$w0rd!";
-      const d = new Date().toISOString();
-      const body = {
-        username: `TestUser_${d}`,
-      };
       const reqBody = {
+        username: `TestUser_${d}`,
         password: password,
-        ...body
       };
       const expStatus = 201;
-      const resBody = body;
+      const resBody = {
+        username: `TestUser_${d}`,
+      };
 
       const res = await supertest(server)
-        .post(apiEndpoint)
+        .post(apiEndpointRegister)
         .send(reqBody)
         .set('Accept', json)
         ;
@@ -51,14 +52,14 @@ describe('server.js', () => {
   });
 
 
-  apiEndpoint = apiBase+'/login';
-  describe('POST '+apiEndpoint, () => {
+  const apiEndpointLogin = apiBase+'/login';
+  describe('POST '+apiEndpointLogin, () => {
 
     it('(missing required data)', async (done) => {
       const reqBody = null;
       const expStatus = 400;
       const resBody = { message: "Required data missing" };
-      const res = await supertest(server).post(apiEndpoint);
+      const res = await supertest(server).post(apiEndpointLogin);
       expect(res.status).toBe(expStatus);
       expect(res.type).toBe(json);
       expect(res.body).toEqual(resBody);
@@ -66,31 +67,32 @@ describe('server.js', () => {
       done();
     });
 
-    // it('(valid)', async (done) => {
-    //   const password = "T3$tP4$$w0rd!";
-    //   const d = new Date().toISOString();
-    //   const reqBody = {
-    //     username: `TestUser_${d}`,
-    //     password: password,
-    //   };
-    //   const expStatus = 200;
-    //   const resBody = {
-        
-    //   };
+    it('(valid)', async (done) => {
+      const u = `TestUser_${d}`;
+      const p = "T3$tP4$$w0rd!";
 
-    //   const res = await supertest(server)
-    //     .post(apiEndpoint)
-    //     .send(reqBody)
-    //     .set('Accept', json)
-    //     ;
+      const reqBody = {
+        username: u,
+        password: p,
+      };
+      const expStatus = 200;
+      const resBody = {
+        message: `Welcome ${u}!`
+      };
 
-    //   expect(res.status).toBe(expStatus);
-    //   expect(res.type).toBe('application/json');
-    //   expect(res.body).toMatchObject(resBody);
-    //   expect(res.body.id).toBeGreaterThanOrEqual(1);
-    //   expect(res.body.password).toMatch(/^\$2a\$\d{1,}\$.*/);
-    //   done();
-    // });
+      const res = await supertest(server)
+        .post(apiEndpointLogin)
+        .send(reqBody)
+        .set('Accept', json)
+        ;
+
+      expect(res.status).toBe(expStatus);
+      expect(res.type).toBe('application/json');
+      expect(res.body).toMatchObject(resBody);
+      expect(res.body.token)
+        .toMatch(/[\w|-]{1,}\.{1}[\w|-]{1,}\.{1}[\w|-]{1,}/);
+      done();
+    });
 
   });
 });
