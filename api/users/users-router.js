b9efdff44ee6104987371c2a7cec656a2b614a85
Paul M Edwards <pauledwards@gmail.com>
2020-02-21 23:42:30 -0800

    Reorder GET / to top: Disable all PUTs: they're broken.

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -18,8 +18,18 @@ router.post('/', auth, (req, res) => {
   };
 });
 
+router.get('/', auth, (req, res) => {
+  Users.readUsers()
+    .then(users => {
+      res.json(users);
+    })
+    .catch(err => {
+      res.status(500).json({ message: `Failed to get users` });
+    });
+});
 router.get('/:userRef', auth, (req, res) => {
   const { userRef } = req.params;
+  console.log(`TCL: get(/:userRef) =`, userRef);
   userId = parseInt(userRef, 10);
   if (userId > 0) {
     if (!process.env.NO_LOGGER) console.log(`TCL: readUserById(${userId})`);
@@ -52,74 +62,91 @@ router.get('/:userRef', auth, (req, res) => {
       });
   };
 });
-router.get('/', auth, (req, res) => {
-  const j = req.decodedJwt;
-  if (!process.env.NO_LOGGER) {
-    console.log(`TCL: req.decodedJwt`, j);
-  };
-  Users.readUsersInRole(roles[0].id)
-    .then(users => {
-      res.json(users);
-    })
-    .catch(err => {
-      res.status(500).json({ message: `Failed to get users` });
-    });
-});
 
-router.put('/:userId/username', auth, (req, res) => {
-  const { userId } = req.params;
-  userId = parseInt(userId, 10);
-  const changes = req.body;
+/* Disabled
+// router.put('/:userId', auth, (req, res) => {
+//   const { userId } = req.params;
+//   uId = parseInt(userId, 10);
+//   const changes = req.body;
 
-  if (!changes.username) {
-    res.status(400).json({ message: `Required data missing` });
-  } else {
-    if (userId > 0) {
-      Users.readUserById(userId)
-        .then(user => {
-          if (!process.env.NO_LOGGER) console.log(`TCL: updateUser(${userId})`);
-          Users.updateUser(userId, changes)
-            .then(updatedUser => {
-              if (updatedUser) {
-                res.json({ updatedUser });
-              } else {
-                res.status(404).json({ message: `Could not get user with given id` });
-              };
-            })
-        })
-        .catch(err => {
-          res.status(500).json({ message: `Failed to update username` });
-        });
-    };
-  };
-});
-router.put('/:userId/password', auth, (req, res) => {
-  const { userId } = req.params;
-  userId = parseInt(userId, 10);
-  const changes = req.body;
+//   if (!changes.username && !changes.password) {
+//     res.status(400).json({ message: `Required data missing` });
+//   } else {
+//     if (uId > 0) {
+//       Users.readUserById(uId)
+//         .then(user => {
+//           if (!process.env.NO_LOGGER) console.log(`TCL: updateUser(${uId})`);
+//           Users.updateUser(uId, changes)
+//             .then(updatedUser => {
+//               if (updatedUser) {
+//                 res.json({ updatedUser });
+//               } else {
+//                 res.status(404).json({ message: `Could not get user with given id` });
+//               };
+//             })
+//         })
+//         .catch(err => {
+//           res.status(500).json({ message: `Failed to update username` });
+//         });
+//     };
+//   };
+// });
 
-  if (!changes.password) {
-    res.status(400).json({ message: `Required data missing` });
-  } else {
-    if (userId > 0) {
-      Users.readUserById(userId)
-        .then(user => {
-          if (!process.env.NO_LOGGER) console.log(`TCL: updateUser(${userId})`);
-          Users.updateUser(userId, changes)
-            .then(updatedUser => {
-              if (updatedUser) {
-                res.json({ updatedUser });
-              } else {
-                res.status(404).json({ message: `Could not get user with given id` });
-              };
-            })
-        })
-        .catch(err => {
-          res.status(500).json({ message: `Failed to update user password` });
-        });
-    };
-  };
-});
+// router.put('/:userId/username', auth, (req, res) => {
+//   const { userId } = req.params;
+//   userId = parseInt(userId, 10);
+//   const changes = req.body;
+
+//   if (!changes.username) {
+//     res.status(400).json({ message: `Required data missing` });
+//   } else {
+//     if (userId > 0) {
+//       Users.readUserById(userId)
+//         .then(user => {
+//           if (!process.env.NO_LOGGER) console.log(`TCL: updateUser(${userId})`);
+//           Users.updateUser(userId, changes)
+//             .then(updatedUser => {
+//               if (updatedUser) {
+//                 res.json({ updatedUser });
+//               } else {
+//                 res.status(404).json({ message: `Could not get user with given id` });
+//               };
+//             })
+//         })
+//         .catch(err => {
+//           res.status(500).json({ message: `Failed to update username` });
+//         });
+//     };
+//   };
+// });
+// router.put('/:userId/password', auth, (req, res) => {
+//   const { userId } = req.params;
+//   userId = parseInt(userId, 10);
+//   const changes = req.body;
+
+//   if (!changes.password) {
+//     res.status(400).json({ message: `Required data missing` });
+//   } else {
+//     if (userId > 0) {
+//       Users.readUserById(userId)
+//         .then(user => {
+//           if (!process.env.NO_LOGGER) console.log(`TCL: updateUser(${userId})`);
+//           Users.updateUser(userId, changes)
+//             .then(updatedUser => {
+//               if (updatedUser) {
+//                 res.json({ updatedUser });
+//               } else {
+//                 res.status(404).json({ message: `Could not get user with given id` });
+//               };
+//             })
+//         })
+//         .catch(err => {
+//           res.status(500).json({ message: `Failed to update user password` });
+//         });
+//     };
+//   };
+// });
+*/
 
 router.delete('/:userId', auth, (req, res) => {
   const { userId } = req.params;
