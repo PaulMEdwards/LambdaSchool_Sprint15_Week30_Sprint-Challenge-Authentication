d3a6819da11c6115be51e9f543c4dcf12c3db8f2
Paul M Edwards <pauledwards@gmail.com>
2020-02-21 20:31:58 -0800

    Fix a few references.

--- api/api-router.js
+++ api/api-router.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
+const authenticate = require('./middleware/authenticate-middleware.js');
 
-const authRouter = require('./auth/auth-router.js.js');
+const authRouter = require('./auth/auth-router.js');
 const jokesRouter = require('./jokes/jokes-router.js');
 
 router.use('/auth', authRouter);
